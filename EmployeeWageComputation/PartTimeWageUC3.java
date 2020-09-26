a0301d6e3beada2ba42d9b47b8c8af63f603f0a9
Ard2238 <abhishekrdas28@gmail.com>
2020-09-26 07:59:55 +0530

    Added to Compute Part Time Employee's Wage

---
+++ EmployeeWageComputation/PartTimeWageUC3.java
@@ -0,0 +1,18 @@
+public class PartTimeWageUC3{
+	public static void main(String args[]){
+
+		int present_fulltime= 1, present_parttime=2, fullday_hours= 0;
+		int perhour_wage = 20;
+		int checkEmp = (int) (Math.random() * 10) % 3;
+		
+		if( checkEmp == present_parttime){
+			fullday_hours = 4;
+		}
+		else if ( checkEmp == present_fulltime){
+			fullday_hours = 8;
+		}
+		
+		int wages = perhour_wage * fullday_hours;
+		System.out.println("The Employee wages are " + wages);
+	}
+}
